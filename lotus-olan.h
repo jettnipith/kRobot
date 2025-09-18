5f5b8f02c11138dccef5c2e7dc635f892ff8efff
jettnipith <35611274+jettnipith@users.noreply.github.com>
2025-09-19 03:46:12 +0700

    Update lotus-olan.h

--- lotus-olan.h
+++ lotus-olan.h
@@ -235,7 +235,7 @@ void bw_no_line(int timeLimiter) {
 void PIDforward() {
   int linePosition = getLinePosition();  // Position from the sensor array
   float error = 2000 - linePosition;     // Target position is center (4000) for 8 sensors
-  if (error == 0) integral = 0;
+  if (error == 0) _integral = 0;
   _integral += error;
   float derivative = error - _previousError;
   
@@ -345,4 +345,5 @@ void wait_SW1_done() {
     }
   }
 }
+
 
