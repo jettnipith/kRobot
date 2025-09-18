3c9726b81b97ec8460e5843dadb01181e89a4b60
jettnipith <35611274+jettnipith@users.noreply.github.com>
2025-09-19 03:45:47 +0700

    Update lotus-esmp.h

--- lotus-esmp.h
+++ lotus-esmp.h
@@ -235,10 +235,10 @@ void bw_no_line(int timeLimiter) {
 void PIDforward() {
   int linePosition = getLinePosition();  // Position from the sensor array
   float error = 2000 - linePosition;     // Target position is center (4000) for 8 sensors
-
+  if (error == 0) _integral = 0;
   _integral += error;
   float derivative = error - _previousError;
-  if (error == 0) integral = 0;
+  
   float correction = _kp * error + _ki * _integral + _kd * derivative;
   _previousError = error;
 
